44d4171fdd5ae5061028d21f234f164c461b481d
vlada <laym83241@gmail.com>
2025-05-05 23:51:58 +0300

    add controllers

---
+++ Controllers/GiftsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GiftGenieAPIWebApp.Models;
+
+namespace GiftGenieAPIWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftsController : ControllerBase
+    {
+        private readonly GiftGenieContext _context;
+
+        public GiftsController(GiftGenieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Gifts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Gift>>> GetGifts()
+        {
+            return await _context.Gifts.ToListAsync();
+        }
+
+        // GET: api/Gifts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Gift>> GetGift(int id)
+        {
+            var gift = await _context.Gifts.FindAsync(id);
+
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return gift;
+        }
+
+        // PUT: api/Gifts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGift(int id, Gift gift)
+        {
+            if (id != gift.GiftId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gift).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GiftExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Gifts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Gift>> PostGift(Gift gift)
+        {
+            _context.Gifts.Add(gift);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGift", new { id = gift.GiftId }, gift);
+        }
+
+        // DELETE: api/Gifts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGift(int id)
+        {
+            var gift = await _context.Gifts.FindAsync(id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gifts.Remove(gift);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GiftExists(int id)
+        {
+            return _context.Gifts.Any(e => e.GiftId == id);
+        }
+    }
+}
